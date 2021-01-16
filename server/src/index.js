a8d83a130f2b4f88b07af9770efac99aac52c9f3
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2021-01-16 03:15:27 -0500

    use firebase auth middleware in index file

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const auth = require('./auth.js');
 
 const app = express();
 const port = 8080;
 
 app.use(cors());
+
+app.use(auth);
 
 app.get('/', (req, res) => {
     res.send("Hello world!");
