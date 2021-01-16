61a2c66d1cce3a8e64e35e5dc9b3ae810ed47d1f
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2021-01-16 18:02:10 -0500

    fix backend

--- server/src/index.js
+++ server/src/index.js
@@ -5,14 +5,37 @@ const bodyParser = require('body-parser');
 const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
 
 const client = new SecretManagerServiceClient();
-const [accessResponse] = client.accessSecretVersion({name: "mongo_link"});
-const mongo_link = accessResponse.payload.data.toString('utf8');
 
 const auth = require('./auth.js');
 const userRoutes = require('./user/routes.js');
 
 const app = express();
 const port = 8080;
+
+async function main() {
+    const [accessResponse] = await client.accessSecretVersion({name: "projects/budgy-301915/secrets/mongo_link/versions/latest"});
+    const mongo_link = accessResponse.payload.data.toString('utf8');
+
+    // Connect to mongoDB
+    mongoose.connect(mongo_link, { useNewUrlParser: true })
+    .then(response => console.log("Connected to MongoDB"))
+    .catch(err => console.log(err));
+
+    app.set("mongoose", mongoose);
+
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    app.use(auth);
+
+    app.get('/', (req, res) => {
+        res.send("Hello world!");
+    });
+
+    app.use("/user", userRoutes)
+
+    app.listen(port);
+}
 
 // Clean up on exit
 function disconnect() {
@@ -25,22 +48,4 @@ process.on("SIGINT", disconnect)
 process.on("SIGHUP", disconnect)
 process.on("SIGTERM", disconnect)
 
-// Connect to mongoDB
-mongoose.connect(mongo_link, { useNewUrlParser: true })
-.then(response => console.log("Connected to MongoDB"))
-.catch(err => console.log(err));
-
-app.set("mongoose", mongoose);
-
-app.use(bodyParser.json());
-app.use(cors());
-
-app.use(auth);
-
-app.get('/', (req, res) => {
-    res.send("Hello world!");
-});
-
-app.use("/user", userRoutes)
-
-app.listen(port);
+main()
