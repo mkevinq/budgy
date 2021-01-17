cd6321669d11f44793a55bb596e0da86800e5cf2
Truman Be <trumanbe01@gmail.com>
2021-01-17 04:58:58 -0500

    Provided some basic formatting for purchases screens

--- App/components/itemInfo.js
+++ App/components/itemInfo.js
@@ -4,9 +4,9 @@ import { View, Text } from 'react-native';
 export default class ItemInfo extends React.Component {
     render() {
         return(
-            <View>
-                <Text>{this.props.name}</Text>
-                <Text>${this.props.cost.toFixed(2)}</Text>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginHorizontal: 10}}>
+                <Text style={{fontSize: 20}}>{this.props.name}</Text>
+                <Text style={{fontSize: 20}}>${this.props.cost.toFixed(2)}</Text>
             </View>
         )
     }
