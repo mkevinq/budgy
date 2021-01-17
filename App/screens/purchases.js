f47dd492e89515a0d56c54c7d91e5b684e6b6816
Truman Be <trumanbe01@gmail.com>
2021-01-16 22:27:56 -0500

    Fixed minor mistake with function call

--- App/screens/purchases.js
+++ App/screens/purchases.js
@@ -6,7 +6,7 @@ export default class Purchases extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            allPurchases: getPurchases
+            allPurchases: getPurchases()
         }
     }
 
