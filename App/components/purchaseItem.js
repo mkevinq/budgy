5ff9885a22d5b0be5b62441f4969947df14ded58
Truman Be <trumanbe01@gmail.com>
2021-01-17 04:34:16 -0500

    Added slight styling for purchases screen

--- App/components/purchaseItem.js
+++ App/components/purchaseItem.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { View, FlatList, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default class PurchaseItem extends React.Component {
     render() {
         return(
-            <View style={{borderBottomWidth: 5}}>
-                <Text>{this.props.location}</Text>
-                <Text>{this.props.date}</Text>
-                <Text>${this.props.total}</Text>
+            <View style={{flexDirection: "row", alignItems: 'center', justifyContent: 'space-between', borderBottomWidth: 2}}>
+                <View>
+                    <Text style={{fontSize: 30}}>{this.props.location}</Text>
+                    <Text style={{}}>{this.props.date}</Text>
+                </View>
+                <Text style={{fontSize: 25}}>${(this.props.total).toFixed(2)}</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    button: {
+        
+    }
+});
