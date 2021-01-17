1a8cf0dd04acd36b89cedb5b90cf496673254a65
Truman Be <trumanbe01@gmail.com>
2021-01-16 22:26:27 -0500

    Modified purchases screen structure

--- App/screens/purchases.js
+++ App/screens/purchases.js
@@ -1,51 +1,58 @@
 import React from 'react';
 import { View, FlatList, Text } from 'react-native';
 import PurchaseItem from '../components/purchaseItem';
+
 export default class Purchases extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            allPurchases: getPurchases
+        }
+    }
+
+    // Retrieving an array of the purchase IDs
+    getPurchases() {
+        fetch('https://budgy-r5enpvgyka-uc.a.run.app', {
+            method: 'GET /user/purchases',
+            headers: {
+                'Authorization': 'Bearer oiwjefoijaweoigfjaosgjaa',
+                'Content-type': 'application/json'
+            }})
+            .then((response) => response.json())
+            .then((json) => {
+                return json.data.purchases;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    // Retrieves information regarding a particular purchase, given the specific ID
+    getPurchaseInformation(id) {
+        fetch('https://budgy-r5enpvgyka-uc.a.run.app', {
+            method: 'GET /user/purchase/' + id,
+            headers: {
+                'Authorization': 'Bearer oiwjefoijaweoigfjaosgjaa',
+                'Content-type': 'application/json'
+            }})
+            .then((response) => response.json())
+            .then((json) => {
+                return json.data;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    mapPurchaseInfo(purchases) {
+        return purchases.map(getPurchaseInformation());
+    }
+    
     render() {
-
-        // Retrieving an array of the purchase IDs
-        const getPurchases = () => {
-            fetch('https://budgy-r5enpvgyka-uc.a.run.app', {
-                method: 'GET /user/purchases',
-                headers: {
-                    'Authorization': 'Bearer oiwjefoijaweoigfjaosgjaa',
-                    'Content-type': 'application/json'
-                }})
-                .then((response) => response.json())
-                .then((json) => {
-                    return json.data.purchases;
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        }
-
-        // Retrieves information regarding a particular purchase, given the specific ID
-        const getPurchaseInformation = (id) => {
-            fetch('https://budgy-r5enpvgyka-uc.a.run.app', {
-                method: 'GET /user/purchase/' + id,
-                headers: {
-                    'Authorization': 'Bearer oiwjefoijaweoigfjaosgjaa',
-                    'Content-type': 'application/json'
-                }})
-                .then((response) => response.json())
-                .then((json) => {
-                    return json.data;
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        }
-
-        const mapPurchaseInfo = (purchases) => {
-            return purchases.map(getPurchaseInformation());
-        }
-
         return(
             <View>
                 <FlatList
-                    data={mapPurchaseInfo(getPurchases())}
+                    data={mapPurchaseInfo(this.state.allPurchases)}
                     renderItem={({item}) => (
                         <PurchaseItem store={item.location} date={item.date} total={item.total} />
                     )}
