2b57f27d9c540c472d3d0c60c280f5fe4eef7f44
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2021-01-20 12:54:53 -0500

    re-enforce and fix authentication middleware

--- server/src/auth.js
+++ server/src/auth.js
@@ -3,23 +3,17 @@ const admin = require('firebase-admin');
 // On Cloud Run, this should automatically be associated with the proper service acc
 const app = admin.initializeApp();
 
-/*
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split("Bearer ")[1];
-        admin.auth().verifyIdToken(token)
-        .then((decoded) => req.user = decoded);
-    } catch (err) {
-        console.log(err);
-        return res.status(401).json({
+    const token = req.headers.authorization.split("Bearer ")[1];
+
+    admin.auth().verifyIdToken(token)
+    .then((decoded) => {
+        req.user = decoded
+        next();
+    })
+    .catch((err) => {
+        res.status(401).json({
             message: "Could not authenticate"
         })
-    }
-    next();
-}
-*/
-
-module.exports = (req, res, next) => {
-    req.user = { uid: "test_user" }
-    next();
+    })
 }
